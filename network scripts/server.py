7fab77df21918d49a5fc5320893052d359dc934f
Andrew Eells <andrew.eells3@gmail.com>
2019-03-21 10:59:19 -0400

    added ability to send and receive circle, line, rectangle script

--- network scripts/server.py
+++ network scripts/server.py
@@ -15,7 +15,10 @@ while True: # wait for robot to connect
     c, addr = s.accept()
     print("connection at", addr)
     print("listening for file at send/script.py")
-    listenPath = Path("send/script.py")
+    listenPath = Path("send/script.py") # for final implementation
+    listenPathLine = Path("send/line.py") # line for demo1
+    listenPathCircle = Path("send/circle.py") # circle for demo1
+    listenPathRectangle = Path("send/rectangle.py") # rectangle for demo1
     while True: # check if there is file to send
         if listenPath.is_file():
             print("file detected, attempting to send")
@@ -27,3 +30,33 @@ while True: # wait for robot to connect
             print("transfer complete, deleting input file")
             f.close()
             os.remove("send/script.py")
+        if listenPathLine.is_file():
+            print("file detected, attempting to send")
+            f = open('send/line.py','rb')
+            l = f.read(1024)
+            while (l):
+                c.send(l)
+                l = f.read(1024)
+            print("transfer complete, deleting input file")
+            f.close()
+            os.remove("send/line.py")
+        if listenPathCircle.is_file():
+            print("file detected, attempting to send")
+            f = open('send/circle.py','rb')
+            l = f.read(1024)
+            while (l):
+                c.send(l)
+                l = f.read(1024)
+            print("transfer complete, deleting input file")
+            f.close()
+            os.remove("send/circle.py")
+        if listenPathRectangle.is_file():
+            print("file detected, attempting to send")
+            f = open('send/rectangle.py','rb')
+            l = f.read(1024)
+            while (l):
+                c.send(l)
+                l = f.read(1024)
+            print("transfer complete, deleting input file")
+            f.close()
+            os.remove("send/rectangle.py")
