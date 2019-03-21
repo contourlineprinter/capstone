524f235a1b7cb37d42f4b21bf53e4e3a39d10552
unknown <jesse.dewald@gmail.com>
2019-03-21 12:56:46 -0400

    bugfixes and exception handling for network scripts

--- network scripts/client.py
+++ network scripts/client.py
@@ -4,6 +4,7 @@ import os
 
 s = socket.socket() # new socket object
 host = "35.239.126.169"
+# host = socket.gethostname()
 port = 377
 s.connect((host,port))
 
@@ -11,11 +12,17 @@ print("established connection to server on", host, port)
 
 while True: # listen for new file data
     l = s.recv(1024)
-    if (l):
+    if (l): # begin receipt of file
         print("receiving file")
+        data = bytearray()
+        while True: # receive file data into memory
+            data += l
+            if data[-3:] == b"EOD": # if end of data
+                data = data[:-3]
+                break
+            else: # not end of data, continue reading as normal
+                l = s.recv(1024)
         f = open('receive/script.py','wb')
-        while (l):
-            f.write(l)
-            l = s.recv(1024)
+        f.write(data)
         f.close()
         print("file transfer complete")
--- network scripts/server.py
+++ network scripts/server.py
@@ -1,6 +1,7 @@
 import socket
 from pathlib import Path
 import os
+import time
 
 s = socket.socket() # new socket object
 host = socket.gethostname()
@@ -21,42 +22,62 @@ while True: # wait for robot to connect
     listenPathRectangle = Path("send/rectangle.py") # rectangle for demo1
     while True: # check if there is file to send
         if listenPath.is_file():
-            print("file detected, attempting to send")
-            f = open('send/script.py','rb')
-            l = f.read(1024)
-            while (l):
-                c.send(l)
+            try:
+                print("file detected, attempting to send")
+                f = open('send/script.py','rb')
                 l = f.read(1024)
-            print("transfer complete, deleting input file")
-            f.close()
-            os.remove("send/script.py")
+                while (l):
+                    c.send(l)
+                    l = f.read(1024)
+                c.send(b"EOD") # end of data indicator
+                print("transfer complete, deleting input file")
+                f.close()
+                os.remove("send/script.py")
+            except:
+                print("error reading file. will reattempt in 1 second")
+                time.sleep(1)
         if listenPathLine.is_file():
-            print("file detected, attempting to send")
-            f = open('send/line.py','rb')
-            l = f.read(1024)
-            while (l):
-                c.send(l)
+            try:
+                print("file detected, attempting to send")
+                f = open('send/line.py','rb')
                 l = f.read(1024)
-            print("transfer complete, deleting input file")
-            f.close()
-            os.remove("send/line.py")
+                while (l):
+                    c.send(l)
+                    l = f.read(1024)
+                c.send(b"EOD") # end of data indicator
+                print("transfer complete, deleting input file")
+                f.close()
+                os.remove("send/line.py")
+            except:
+                print("error reading file. will reattempt in 1 second")
+                time.sleep(1)
         if listenPathCircle.is_file():
-            print("file detected, attempting to send")
-            f = open('send/circle.py','rb')
-            l = f.read(1024)
-            while (l):
-                c.send(l)
+            try:
+                print("file detected, attempting to send")
+                f = open('send/circle.py','rb')
                 l = f.read(1024)
-            print("transfer complete, deleting input file")
-            f.close()
-            os.remove("send/circle.py")
+                while (l):
+                    c.send(l)
+                    l = f.read(1024)
+                c.send(b"EOD") # end of data indicator
+                print("transfer complete, deleting input file")
+                f.close()
+                os.remove("send/circle.py")
+            except:
+                print("error reading file. will reattempt in 1 second")
+                time.sleep(1)
         if listenPathRectangle.is_file():
-            print("file detected, attempting to send")
-            f = open('send/rectangle.py','rb')
-            l = f.read(1024)
-            while (l):
-                c.send(l)
+            try:
+                print("file detected, attempting to send")
+                f = open('send/rectangle.py','rb')
                 l = f.read(1024)
-            print("transfer complete, deleting input file")
-            f.close()
-            os.remove("send/rectangle.py")
+                while (l):
+                    c.send(l)
+                    l = f.read(1024)
+                c.send(b"EOD") # end of data indicator
+                print("transfer complete, deleting input file")
+                f.close()
+                os.remove("send/rectangle.py")
+            except:
+                print("error reading file. will reattempt in 1 second")
+                time.sleep(1)
