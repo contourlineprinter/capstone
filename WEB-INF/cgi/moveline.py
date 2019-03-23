f088f2ada0e4791f0da2222ac340eccfd43e8ecb
root <root@printer-vm.us-central1-f.c.printer-234615.internal>
2019-03-23 22:05:56 +0000

    all move scripts work now

--- WEB-INF/cgi/moveline.py
+++ WEB-INF/cgi/moveline.py
@@ -6,9 +6,9 @@ import shutil
 # determine the appropriate file to copy here
 srcfile = "../../robot/examples/Line.py"
 # path to the send folder
-dstroot = "../../network/send"
+dstroot = "../../network/send/line.py"
 
 #assert not os.path.isabs(srcfile)
 #dstdir =  os.path.join(dstroot, os.path.dirname(srcfile))
-shutil.copy(srcfile, dstdir)
+shutil.copy(srcfile, dstroot)
 
