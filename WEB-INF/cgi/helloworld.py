9e917de3b4328ca8fba85d46e53d0a6ce80eb00c
unknown <jesse.dewald@gmail.com>
2019-03-23 16:22:44 -0400

    hhhhhhhhhh

--- WEB-INF/cgi/helloworld.py
+++ WEB-INF/cgi/helloworld.py
@@ -1,5 +1,9 @@
-print "Content-type: text/html\n\n";
+print("Content-type: text/html\n\n");
 count = 0
 while (count < 3):     
     count = count + 1
-    print("Hello Geek")
+    print("WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOW!\n")
+
+f = open('itworked.txt','w')
+f.write("it worked.")
+f.close()
