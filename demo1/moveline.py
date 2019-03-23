33412fd56bce982fcdee0fa0068d162d4330fb60
root <root@printer-vm.us-central1-f.c.printer-234615.internal>
2019-03-23 18:51:06 +0000

    move/copy shape python scripts working

--- demo1/moveline.py
+++ demo1/moveline.py
@@ -4,9 +4,9 @@ import os
 import shutil
 
 # determine the appropriate file to copy here
-srcfile = "/home/contourlineprinter/py_scripts/line.py"
-# path to the send folder 
-destroot = "/home/contourlineprinter/py_scripts/send"
+srcfile = "./line.py"
+# path to the send folder
+dstroot = "./test"
 
 assert not os.path.isabs(srcfile)
 dstdir =  os.path.join(dstroot, os.path.dirname(srcfile))
