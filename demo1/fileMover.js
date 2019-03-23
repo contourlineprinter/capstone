dc8ac9a252137a66fe9b2a5aede0044e2c469601
root <root@printer-vm.us-central1-f.c.printer-234615.internal>
2019-03-23 19:01:31 +0000

    working on fileMover.js

--- demo1/fileMover.js
+++ demo1/fileMover.js
@@ -1,8 +1,10 @@
+// need to write javascript to post to a different spot for each shape
+// need to have result on server side for moving the file.
 function shape(shapetype){
 	input = shapetype
 	$.ajax({
 		type: "POST",
-		url: "/demo1/testscript.py",
+		url: "/demo1/moveline.py",
 		data: { param: input}
 		}).done(function(o) {
 		    console.log('shape ' + input + ' sent');
