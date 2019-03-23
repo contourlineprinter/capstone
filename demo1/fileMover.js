889038e6f74674bc099d916bc1c4187d10982e59
unknown <jesse.dewald@gmail.com>
2019-03-23 17:13:51 -0400

    it worked

--- demo1/fileMover.js
+++ demo1/fileMover.js
@@ -4,7 +4,7 @@ function shape(shapetype){
 	input = shapetype
 	$.ajax({
 		type: "POST",
-		url: "/demo1/helloworld.py",
+		url: "/cgi-bin/helloworld.py",
 		data: { param: input}
 		}).done(function(o) {
 		    console.log('shape ' + input + ' sent');
--- demo1/helloworld.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-print "Content-type: text/html\n\n";
-print "Hello, world!\n"
