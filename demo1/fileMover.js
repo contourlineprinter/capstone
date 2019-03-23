11b724c3331b4b6882339efe0700982a2e736ef6
kenny <kennysexton4@gmail.com>
2019-03-23 17:25:39 -0400

    edit filemover to call scripts based on what button is clicked

--- demo1/fileMover.js
+++ demo1/fileMover.js
@@ -2,12 +2,40 @@
 // need to have result on server side for moving the file.
 function shape(shapetype){
 	input = shapetype
-	$.ajax({
-		type: "POST",
-		url: "/cgi-bin/helloworld.py",
-		data: { param: input}
-		}).done(function(o) {
-		    console.log('shape ' + input + ' sent');
-				console.log(o);
+	if (shapetype == 0){ //line
+		$.ajax({
+			type: "POST",
+			url: "/cgi-bin/helloworld.py",
+			data: { param: input}
+			}).done(function(o) {
+			    console.log('shape ' + input + ' sent');
+					console.log(o);
 		});
+		else if (shapetype == 1){ //circle
+			$.ajax({
+				type: "POST",
+				url: "/cgi-bin/helloworld.py",
+				data: { param: input}
+				}).done(function(o) {
+				    console.log('shape ' + input + ' sent');
+						console.log(o);
+				});
+		}
+		else if (shapetype == 2) { //rectangle
+			$.ajax({
+				type: "POST",
+				url: "/cgi-bin/helloworld.py",
+				data: { param: input}
+				}).done(function(o) {
+				    console.log('shape ' + input + ' sent');
+						console.log(o);
+				});
+		
+		}
+		else {
+			console.log( shapetype + " is not a valid shape");
+		}
+
+	}
+
 }
