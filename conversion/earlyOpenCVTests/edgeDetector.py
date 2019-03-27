97191da33aab20ba8d0275abe3d1ebc566f4ed1f
root <root@printer-vm.us-central1-f.c.printer-234615.internal>
2019-03-27 23:14:52 +0000

    cv2 should be working for python3

--- conversion/earlyOpenCVTests/edgeDetector.py
+++ conversion/earlyOpenCVTests/edgeDetector.py
@@ -1,9 +1,11 @@
 import cv2
+print (cv2.__version__)
+
 
 img = cv2.imread("test.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 blur = cv2.medianBlur(gray, 3)
 edged = cv2.Canny(blur, 600, 100)
 
-cv2.imshow("res", edged);cv2.waitKey();cv2.destroyAllWindows()
+# cv2.imshow("res", edged);cv2.waitKey();cv2.destroyAllWindows()
 cv2.imwrite("res.png", edged)
