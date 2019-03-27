c5c910f1e1b39e69254f3acacb495da59c438018
root <root@printer-vm.us-central1-f.c.printer-234615.internal>
2019-03-27 22:40:49 +0000

    changes

--- conversion/earlyOpenCVTests/edgeDetector.py
+++ conversion/earlyOpenCVTests/edgeDetector.py
@@ -1,8 +1,6 @@
-import numpy as np
 import cv2
-from matplotlib import pyplot as plt
 
-img = cv2.imread("test2.jpg")
+img = cv2.imread("test.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 blur = cv2.medianBlur(gray, 3)
 edged = cv2.Canny(blur, 600, 100)
