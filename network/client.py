2d27e138841cfcbffd55875e690a0622111ecc75
unknown <jesse.dewald@gmail.com>
2019-03-31 16:00:52 -0400

    network scripts update

--- network/client.py
+++ network/client.py
@@ -1,29 +1,49 @@
 import socket
 from pathlib import Path
 import os
+import time
 
-s = socket.socket() # new socket object
-host = "35.239.126.169"
-# host = socket.gethostname()
-port = 377
-s.connect((host,port))
+def newConnection():
 
-print("established connection to server on", host, port)
+    s = socket.socket() # new socket object
+    host = "35.239.126.169"
+    # host = socket.gethostname()
+    port = 377
 
-while True: # listen for new file data
-    l = s.recv(1024)
-    if (l): # begin receipt of file
-        print("receiving file")
-        data = bytearray()
-        while True: # receive file data into memory
-            data += l
-            if data[-3:] == b"EOD": # if end of data
-                data = data[:-3]
-                break
-            else: # not end of data, continue reading as normal
-                l = s.recv(1024)
-        f = open('receive/script.py','wb')
-        f.write(data)
-        f.close()
-        print("file transfer complete, executing script")
-        exec(open("./receive/script.py").read())
+    while True: # attempt to make connection to server
+        try:
+            s.connect((host,port))
+            break
+        except:
+            print("host refused connection, reattempting in 1 second...")
+            time.sleep(1)
+
+    print("established connection to server on", host, port)
+
+    try: # this is the main loop, if any error comes up we attempt to reconnect
+        while True: # listen for new file data
+            l = s.recv(1024)
+            if (l): # begin receipt of file
+                print("receiving file")
+                data = bytearray()
+                while True: # receive file data into memory
+                    data += l
+                    if data[-3:] == b"EOD": # if end of data
+                        data = data[:-3]
+                        break
+                    else: # not end of data, continue reading as normal
+                        l = s.recv(1024)
+                f = open('receive/script.py','wb')
+                f.write(data)
+                f.close()
+                print("file transfer complete, executing script")
+                exec(open("./receive/script.py").read())
+    except:
+        print("error! attempting to reconnect...")
+        s.shutdown(socket.SHUT_RDWR)
+        s.close()
+        return
+
+while True:
+    newConnection()
+    # every time connection is lost, we attempt to reconnect
--- network/server.py
+++ network/server.py
@@ -3,81 +3,48 @@ from pathlib import Path
 import os
 import time
 
-s = socket.socket() # new socket object
-host = socket.gethostname()
-print (host)
-port = 377
-s.bind((host,port))
+def newConnection():
 
-print("now listening on", host, port)
-s.listen()
+    s = socket.socket() # new socket object
+    host = socket.gethostname()
+    port = 377
+    s.bind((host,port))
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # allows faster reconnects
+    print("now listening on", host, port)
+    s.listen()
 
-while True: # wait for robot to connect
-    c, addr = s.accept()
-    print("connection at", addr)
-    print("listening for file at send/script.py")
-    listenPath = Path("send/script.py") # for final implementation
-    listenPathLine = Path("send/line.py") # line for demo1
-    listenPathCircle = Path("send/circle.py") # circle for demo1
-    listenPathRectangle = Path("send/rectangle.py") # rectangle for demo1
-    while True: # check if there is file to send
-        if listenPath.is_file():
-            try:
-                print("file detected, attempting to send")
-                f = open('send/script.py','rb')
-                l = f.read(1024)
-                while (l):
-                    c.send(l)
+    while True: # wait for robot to connect
+        c, addr = s.accept()
+        print("connection at", addr)
+        print("listening for file at send/script.py")
+        listenPath = Path("send/script.py") # for final implementation
+        listenPathLine = Path("send/line.py") # line for demo1
+        listenPathCircle = Path("send/circle.py") # circle for demo1
+        listenPathRectangle = Path("send/rectangle.py") # rectangle for demo1
+        while True:
+            try: # check if client disconnect
+                c.send(bytes("","UTF-8")) 
+            except socket.error: 
+                print('client disconnected')
+                c.shutdown(socket.SHUT_RDWR)
+                c.close()
+                return
+            if listenPath.is_file(): # check if there is file to send
+                try:
+                    print("file detected, attempting to send")
+                    f = open('send/script.py','rb')
                     l = f.read(1024)
-                c.send(b"EOD") # end of data indicator
-                print("transfer complete, deleting input file")
-                f.close()
-                os.remove("send/script.py")
-            except:
-                print("error reading file. will reattempt in 1 second")
-                time.sleep(1)
-        if listenPathLine.is_file():
-            try:
-                print("file detected, attempting to send")
-                f = open('send/line.py','rb')
-                l = f.read(1024)
-                while (l):
-                    c.send(l)
-                    l = f.read(1024)
-                c.send(b"EOD") # end of data indicator
-                print("transfer complete, deleting input file")
-                f.close()
-                os.remove("send/line.py")
-            except:
-                print("error reading file. will reattempt in 1 second")
-                time.sleep(1)
-        if listenPathCircle.is_file():
-            try:
-                print("file detected, attempting to send")
-                f = open('send/circle.py','rb')
-                l = f.read(1024)
-                while (l):
-                    c.send(l)
-                    l = f.read(1024)
-                c.send(b"EOD") # end of data indicator
-                print("transfer complete, deleting input file")
-                f.close()
-                os.remove("send/circle.py")
-            except:
-                print("error reading file. will reattempt in 1 second")
-                time.sleep(1)
-        if listenPathRectangle.is_file():
-            try:
-                print("file detected, attempting to send")
-                f = open('send/rectangle.py','rb')
-                l = f.read(1024)
-                while (l):
-                    c.send(l)
-                    l = f.read(1024)
-                c.send(b"EOD") # end of data indicator
-                print("transfer complete, deleting input file")
-                f.close()
-                os.remove("send/rectangle.py")
-            except:
-                print("error reading file. will reattempt in 1 second")
-                time.sleep(1)
+                    while (l):
+                        c.send(l)
+                        l = f.read(1024)
+                    c.send(b"EOD") # end of data indicator
+                    print("transfer complete, deleting input file")
+                    f.close()
+                    os.remove("send/script.py")
+                except:
+                    print("error reading file. will reattempt in 1 second")
+                    time.sleep(1)
+
+while True:
+    newConnection()
+    # every time connection is lost, we attempt to reconnect
