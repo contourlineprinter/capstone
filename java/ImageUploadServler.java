0ca100654dbaddf376929a9679784309224631cb
kennysexton <contourlineprinter@gmail.com>
2019-04-02 03:57:19 +0000

    still having issues getting the python code to execute on upload,  need to look at the server logs to find out errors

--- java/ImageUploadServler.java
+++ java/ImageUploadServler.java
@@ -14,45 +14,68 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 public class ImageUploadServler extends HttpServlet {
-	private static final long serialVersionUID = 1L;	
+	// static final long serialVersionUID = 1L;
 	private String file_directory;
+	public String name;
 
-    public ImageUploadServler() {
-        super();
-    }
-    
-    @Override
-    public void init() throws ServletException {
-    	file_directory = getServletContext().getRealPath("/images");
-    }
+	public ImageUploadServler() {
+		super();
+	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	public void init() throws ServletException {
+		file_directory = getServletContext().getRealPath("/images");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if(ServletFileUpload.isMultipartContent(request)){
-            try {
-                List<FileItem> multiparts = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
-               
-                for(FileItem item : multiparts){
-                    if(!item.isFormField()){
-                        String name = new File(item.getName()).getName();
-                        item.write( new File(file_directory + File.separator + name));
-                    }
-                }
-               request.setAttribute("message", "Image uploaded successfully");
-            } catch (Exception ex) {
-               request.setAttribute("message", "Image upload failed due to: " + ex);
-               ex.printStackTrace();
-            }          
-          
-        }else{
-            request.setAttribute("message",
-                                 "Unable to upload the file");
-        }
-     
-        getServletContext().getRequestDispatcher("/result.jsp").include(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		if (ServletFileUpload.isMultipartContent(request)) {
+			try {
+				List<FileItem> multiparts = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+
+				for (FileItem item : multiparts) {
+					if (!item.isFormField()) {
+						name = new File(item.getName()).getName();
+						item.write(new File(file_directory + File.separator + name));
+					}
+				}
+				//Upload Worked
+				request.setAttribute("message", "Image uploaded successfully");
+					
+				
+			} catch (Exception ex) {
+				request.setAttribute("message", "Image upload failed due to: " + ex);
+				ex.printStackTrace();
+			}
+
+		} else {
+			request.setAttribute("message", "Unable to upload the file");
+		}
+		
+		
+		//Call python script on file
+	    try {
+	    	Process p = Runtime.getRuntime().exec("python3 /conversion/imageConverter/convertFileInput.py ../../images/" + name);
+	        if(p.waitFor() == 0 ){
+	            System.out.println("Process terminated ");
+	        }
+	    } catch (IOException e) {
+	    	request.setAttribute("message", "IOException: " + e);
+	    	e.printStackTrace();
+	    } catch (InterruptedException e) {
+	    	request.setAttribute("message", "InterruptedException: " + e);
+	    	e.printStackTrace();
+	    }
+	    
+	    //request.setAttribute("message", "test test test");
+	    
+		// return to Index.jsp page with the response text
+		getServletContext().getRequestDispatcher("/index.jsp").include(request, response);
 	}
 
 }
