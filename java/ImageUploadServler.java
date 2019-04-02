6a60976f3af0385dc5691fb9715a0a07f2aff533
kenny <kennysexton4@gmail.com>
2019-04-01 22:54:35 -0400

    testing conversion on upload part1

--- java/ImageUploadServler.java
+++ java/ImageUploadServler.java
@@ -14,45 +14,52 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 public class ImageUploadServler extends HttpServlet {
-	private static final long serialVersionUID = 1L;	
+	// static final long serialVersionUID = 1L;
 	private String file_directory;
 
-    public ImageUploadServler() {
-        super();
-    }
-    
-    @Override
-    public void init() throws ServletException {
-    	file_directory = getServletContext().getRealPath("/images");
-    }
+	public ImageUploadServler() {
+		super();
+	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	public void init() throws ServletException {
+		file_directory = getServletContext().getRealPath("/images");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if(ServletFileUpload.isMultipartContent(request)){
-            try {
-                List<FileItem> multiparts = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
-               
-                for(FileItem item : multiparts){
-                    if(!item.isFormField()){
-                        String name = new File(item.getName()).getName();
-                        item.write( new File(file_directory + File.separator + name));
-                    }
-                }
-               request.setAttribute("message", "Image uploaded successfully");
-            } catch (Exception ex) {
-               request.setAttribute("message", "Image upload failed due to: " + ex);
-               ex.printStackTrace();
-            }          
-          
-        }else{
-            request.setAttribute("message",
-                                 "Unable to upload the file");
-        }
-     
-        getServletContext().getRequestDispatcher("/result.jsp").include(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		if (ServletFileUpload.isMultipartContent(request)) {
+			try {
+				List<FileItem> multiparts = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+
+				for (FileItem item : multiparts) {
+					if (!item.isFormField()) {
+						String name = new File(item.getName()).getName();
+						item.write(new File(file_directory + File.separator + name));
+					}
+				}
+				//Upload Worked
+				request.setAttribute("message", "Image uploaded successfully");
+				
+				//Call python script on file
+				Process p = Runtime.getRuntime().exec("python3 /conversion/imageConverter/convertFileInput.py");
+				
+			} catch (Exception ex) {
+				request.setAttribute("message", "Image upload failed due to: " + ex);
+				ex.printStackTrace();
+			}
+
+		} else {
+			request.setAttribute("message", "Unable to upload the file");
+		}
+
+		// return to Index.jsp page with the response text
+		getServletContext().getRequestDispatcher("/index.jsp").include(request, response);
 	}
 
 }
