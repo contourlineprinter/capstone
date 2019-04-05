3373424e2b77b7a2e5d621be28653bc651160dd0
root <root@printer-server-vm.us-east1-b.c.striped-smile-236718.internal>
2019-04-05 20:13:03 +0000

    re-wrote dummyTest.py to execute on ./

--- conversion/imageConverter/dummyTest.py
+++ conversion/imageConverter/dummyTest.py
@@ -1,10 +1,7 @@
-# !/usr/bin/env python3
-# creating a simple python script that creates a text file
-# this will be used to test if our server is able to call this on file upload
-file = open("test.txt","w")
-file.write("test \n")
-file.write("1 , 2, 3")
-
+#!/usr/bin/env python3
+# simple python script that createes a text file
+file = open("dummy.txt","w")
+file.write("test\n")
+file.write("1, 2, 3")
 
 file.close()
-
