05238a10787abf3d488ecf0f77c33381774586b7
unknown <jesse.dewald@gmail.com>
2019-04-06 11:29:27 -0400

    networking socket bug fix

--- network/server.py
+++ network/server.py
@@ -16,7 +16,9 @@ def newConnection():
     s.listen()
 
     while True: # wait for robot to connect
+        s.settimeout(None)
         c, addr = s.accept()
+        s.settimeout(2)
         print("connection at", addr)
         print("listening for file at send/script.py")
         listenPath = Path("send/script.py") # for final implementation
