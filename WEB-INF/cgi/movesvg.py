849ea58c0ad7522c21393c741876ad44ce7e06e8
root <root@printer-server-vm.us-east1-b.c.striped-smile-236718.internal>
2019-04-07 23:29:40 +0000

    button not triggering but script should work

--- WEB-INF/cgi/movesvg.py
+++ WEB-INF/cgi/movesvg.py
@@ -1,9 +1,15 @@
-#move svg into send folder
+ #move svg into send folder
 
 import os
 import shutil
+import sys
+from svg_to_instruction import robot_convert
 
-srcfile = "../../svg/banana.jpg_SVG1.svg"
-dstroot = "../../network/send/image.svg"
+# srcfile = "../../svg/banana.jpg_SVG1.svg"
+# dstroot = "../../network/send/image.svg"
 
-shutil.copy(srcfile, dstroot)
+# shutil.copy(srcfile, dstroot)
+
+robot_convert("/var/lib/tomcat8/webapps/ROOT/svg/banana.jpg_SVG1.svg")
+
+
