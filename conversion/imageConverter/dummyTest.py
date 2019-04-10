1d42c1921191238bfb5e797633c938836032d76f
root <root@tomcat-1-vm.us-east1-b.c.contour3.internal>
2019-04-10 21:16:56 +0000

    cleanup.  deleting unused content

--- conversion/imageConverter/dummyTest.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# simple python script that createes a text file
-file = open("/var/lib/tomcat8/webapps/ROOT/dummy.txt","w")
-file.write("test\n")
-file.write("1, 2, 3")
-
-file.close()
