76b5d9ccf90dd2e5cdd946922741be27690b1815
kennysexton <root@tomcat-1-vm.us-east1-b.c.contour3.internal>
2019-04-18 15:33:48 +0000

    added new js file

---
+++ js/selectFromGallery.js
@@ -0,0 +1,9 @@
+// called when an image is clicked on from the gallery
+// Redierects to staging with the selected image passed as input
+
+	
+$(document).ready(function(){
+	$( ".row" ).click(function() {
+		//alert($('.row img:nth-child(1)').attr('src'));
+	});
+});
