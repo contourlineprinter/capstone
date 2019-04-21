c3ee81a68cff01aa3be351e942ae17d64ec1e840
yummiecrossiant <swmagive8@gmail.com>
2019-04-21 18:49:21 -0400

    forgot to import math

--- conversion/imageConverter/ImageConversionClass.py
+++ conversion/imageConverter/ImageConversionClass.py
@@ -4,6 +4,7 @@ import cv2
 import matplotlib.pyplot as plt
 import svgwrite
 import os, sys, ntpath, traceback
+import math
 
 class ImageConversion:    
     "Class to perform image conversion to contour, svg, and robot instructions\n"
