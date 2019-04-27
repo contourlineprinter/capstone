d353f772bca66908837e3606c38d294f84975af7
Andrew Ditty <AndrewDitty@Gmail.com>
2019-04-27 14:23:48 -0400

    except

--- WEB-INF/cgi/movesvg.py
+++ WEB-INF/cgi/movesvg.py
@@ -20,7 +20,7 @@ try:
     #open the file we're writing to
     with open('/var/lib/tomcat8/webapps/ROOT/next/scale.txt','r') as file:
         num = float(file.read().trim())
-except Exception:
+except:
     pass # suppressing errors is not cool, but if there is an error, then use default scale = 1
 
 robot_convert("/var/lib/tomcat8/webapps/ROOT/next/imageSVG.svg",scale)
