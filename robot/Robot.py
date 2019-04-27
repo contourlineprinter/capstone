7173f800de6cec8be456d13d30dbbfba613bb822
Andrew Ditty <AndrewDitty@Gmail.com>
2019-04-27 12:08:22 -0400

    Updated to the Stepper Version
    This has lived on the Robot and gone through many changes there, now I'm moving it here

--- robot/Robot.py
+++ robot/Robot.py
@@ -1,175 +1,199 @@
-# Simple two DC motor robot class.  Exposes a simple LOGO turtle-like API for
-# moving a robot forward, backward, and turning.  See RobotTest.py for an
-# example of using this class.
-# Author: Tony DiCola
-# License: MIT License https://opensource.org/licenses/MIT
+#!/usr/bin/python
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafru$
+import RPi.GPIO as GPIO
 import time
 import atexit
+import threading
 
-from Adafruit_MotorHAT import Adafruit_MotorHAT
-
-SCALE = .0145625
-SLEEP = 0.1
-
+STEPS_PER_ROTATION = 828
 
 class Robot(object):
-    def __init__(self, addr=0x60, left_id=1, right_id=2, left_trim=0, right_trim=0,
-                 stop_at_exit=True):
-        """Create an instance of the robot.  Can specify the following optional
-        parameters:
-         - addr: The I2C address of the motor HAT, default is 0x60.
-         - left_id: The ID of the left motor, default is 1.
-         - right_id: The ID of the right motor, default is 2.
-         - left_trim: Amount to offset the speed of the left motor, can be positive
-                      or negative and use useful for matching the speed of both
-                      motors.  Default is 0.
-         - right_trim: Amount to offset the speed of the right motor (see above).
-         - stop_at_exit: Boolean to indicate if the motors should stop on program
-                         exit.  Default is True (highly recommended to keep this
-                         value to prevent damage to the bot on program crash!).
-        """
-        # Initialize motor HAT and left, right motor.
-        self._mh = Adafruit_MotorHAT(addr)
-        self._left = self._mh.getMotor(left_id)
-        self._right = self._mh.getMotor(right_id)
-        self._left_trim = left_trim
-        self._right_trim = right_trim
+
+    def __init__(self, speed=30, stepstyle='double', stop_at_exit=True, n$
+        self.marker = Marker(15,7,11,13)
+        self.marker.standby()
+        time.sleep(naptime)
+        self.mh = Adafruit_MotorHAT()
+        self.mleft = self.mh.getStepper(513, 1)
+        self.mright = self.mh.getStepper(513, 2)
+        self.speed = speed
+        self.mleft.setSpeed(self.speed)
+        self.mright.setSpeed(self.speed)
+        self.naptime = naptime
+
+        # create empty threads (these will hold the stepper 1 and 2 threa$
+        self.st1 = threading.Thread()
+        self.st2 = threading.Thread()
+
+        # Other option could be  Adafruit_MotorHAT.INTERLEAVE, Adafruit_M$
+        # but for our application single and double are only appropriate
+        if stepstyle.lower() == 'single':
+            self.stepstyle = Adafruit_MotorHAT.SINGLE
+        else:
+            self.stepstyle = Adafruit_MotorHAT.DOUBLE
+
+
         # Start with motors turned off.
-        self._left.run(Adafruit_MotorHAT.RELEASE)
-        self._right.run(Adafruit_MotorHAT.RELEASE)
+        #self.mleft.run(Adafruit_MotorHAT.RELEASE)
+        #self.mright.run(Adafruit_MotorHAT.RELEASE)
         # Configure all motors to stop at program exit if desired.
-        if stop_at_exit:
-            atexit.register(self.stop)
+        #if stop_at_exit:
+        #   atexit.register(self.stop)
 
-    def _left_speed(self, speed):
-        """Set the speed of the left motor, taking into account its trim offset.
-        """
-        assert 0 <= speed <= 255, 'Speed must be a value between 0 to 255 inclusive!'
-        speed += self._left_trim
-        speed = max(0, min(255, speed))  # Constrain speed to 0-255 after trimming.
-        self._left.setSpeed(speed)
+    def stepper_worker(self, stepper, numsteps, direction, style):
+        stepper.step(numsteps, direction, style)
 
-    def _right_speed(self, speed):
-        """Set the speed of the right motor, taking into account its trim offset.
-        """
-        assert 0 <= speed <= 255, 'Speed must be a value between 0 to 255 inclusive!'
-        speed += self._right_trim
-        speed = max(0, min(255, speed))  # Constrain speed to 0-255 after trimming.
-        self._right.setSpeed(speed)
+    # recommended for auto-disabling motors on shutdown!
+    def turnOffMotors(self):
+        self.mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+        self.mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+        self.mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+        self.mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+        #atexit.register(turnOffMotors)
 
-    def stop(self):
-        """Stop all movement."""
-        self._left.run(Adafruit_MotorHAT.RELEASE)
-        self._right.run(Adafruit_MotorHAT.RELEASE)
+    def forward(self, steps):
+        print('forward start')
+        self.marker.down()
+        if not self.st1.isAlive():
+            self.st1 = threading.Thread(target=self.stepper_worker, args=$
 
-    def forward(self, speed, seconds=None):
-        """Move forward at the specified speed (0-255).  Will start moving
-        forward and return unless a seconds value is specified, in which
-        case the robot will move forward for that amount of time and then stop.
-        """
-        # Set motor speed and move both forward.
-        self._left_speed(speed)
-        self._right_speed(speed)
-        self._left.run(Adafruit_MotorHAT.FORWARD)
-        self._right.run(Adafruit_MotorHAT.FORWARD)
-        # If an amount of time is specified, move for that time and then stop.
-        if seconds is not None:
-            time.sleep(seconds)
-            self.stop()
+        if not self.st2.isAlive():
+            self.st2 = threading.Thread(target=self.stepper_worker, args=$
+        time.sleep(0.2)
+        self.st2.start()
+        self.st1.start()
 
-    def backward(self, speed, seconds=None):
-        """Move backward at the specified speed (0-255).  Will start moving
-        backward and return unless a seconds value is specified, in which
-        case the robot will move backward for that amount of time and then stop.
-        """
-        # Set motor speed and move both backward.
-        self._left_speed(speed)
-        self._right_speed(speed)
-        self._left.run(Adafruit_MotorHAT.BACKWARD)
-        self._right.run(Adafruit_MotorHAT.BACKWARD)
-        # If an amount of time is specified, move for that time and then stop.
-        if seconds is not None:
-            time.sleep(seconds)
-            self.stop()
+        self.st1.join()
+        self.st2.join()
+        time.sleep(self.naptime)
+        print('forward end')
 
-    def right(self, speed, seconds=None):
-        """Spin to the right at the specified speed.  Will start spinning and
-        return unless a seconds value is specified, in which case the robot will
-        spin for that amount of time and then stop.
-        """
-        # Set motor speed and move both forward.
-        self._left_speed(speed)
-        self._right_speed(speed)
-        self._left.run(Adafruit_MotorHAT.FORWARD)
-        self._right.run(Adafruit_MotorHAT.BACKWARD)
-        # If an amount of time is specified, move for that time and then stop.
-        if seconds is not None:
-            time.sleep(seconds)
-            self.stop()
+    def move(self, steps):
+        print('move start')
+        self.marker.up()
+        if not self.st1.isAlive():
+            self.st1 = threading.Thread(target=self.stepper_worker, args=$
 
-    def left(self, speed, seconds=None):
-        """Spin to the left at the specified speed.  Will start spinning and
-        return unless a seconds value is specified, in which case the robot will
-        spin for that amount of time and then stop.
-        """
-        # Set motor speed and move both forward.
-        self._left_speed(speed)
-        self._right_speed(speed)
-        self._left.run(Adafruit_MotorHAT.BACKWARD)
-        self._right.run(Adafruit_MotorHAT.FORWARD)
-        # If an amount of time is specified, move for that time and then stop.
-        if seconds is not None:
-            time.sleep(seconds)
-            self.stop()
+        if not self.st2.isAlive():
+            self.st2 = threading.Thread(target=self.stepper_worker, args=$
+        time.sleep(0.2)
+        self.st2.start()
+        self.st1.start()
 
-    def arcLeft(self, speed, seconds=None):
-        self._left_speed(speed/2)
-        self._right_speed(speed)
-        self._left.run(Adafruit_MotorHAT.FORWARD)
-        self._right.run(Adafruit_MotorHAT.FORWARD)
-        if seconds is not None:
-            time.sleep(seconds)
-            self.stop()
+        self.st1.join()
+        self.st2.join()
+        self.marker.down()
+        time.sleep(self.naptime)
+        print('move end')
 
-    def arc(self, l_speed, r_speed, seconds=None):
-        self._left_speed(l_speed)
-        self._right_speed(r_speed)
-        self._left.run(Adafruit_MotorHAT.FORWARD)
-        self._right.run(Adafruit_MotorHAT.FORWARD)
-        if seconds is not None:
-            time.sleep(seconds)
-            self.stop()
+    def right(self, steps):
+        print('right start')
+        self.marker.up()
+        if not self.st1.isAlive():
+            self.st1 = threading.Thread(target=self.stepper_worker, args=$
+
+        if not self.st2.isAlive():
+            self.st2 = threading.Thread(target=self.stepper_worker, args=$
+        time.sleep(0.2)
+        self.st2.start()
+        self.st1.start()
+        #time.sleep(0.1)
+        self.st1.join()
+        self.st2.join()
+        self.marker.down()
+        time.sleep(self.naptime)
+        print('right end')
+
+
+    def left(self, steps):
+        print('left start')
+        self.marker.up()
+        if not self.st1.isAlive():
+            self.st1 = threading.Thread(target=self.stepper_worker, args=$
+
+        if not self.st2.isAlive():
+            self.st2 = threading.Thread(target=self.stepper_worker, args=$
+        time.sleep(0.2)
+        self.st2.start()
+        self.st1.start()
+        # time.sleep(0.1)
+        self.st1.join()
+        self.st2.join()
+        self.marker.down()
+        time.sleep(self.naptime)
+        print('left end')
+
 
     def rotate(self, theta):
-        time.sleep(SLEEP)
-        self._left_speed(50)
-        self._right_speed(50)
-
         #make theta make sense
+
+        while theta >= 360:
+            theta = theta - 360
+
         if theta > 180:
-           theta = -1 * (360-theta)
+            theta = -1 * (360-theta)
+        elif theta < -180:
+            theta = 360+theta
 
-        rots = abs(int(theta/45))
-        rem = theta%45
+        steps = int((theta * STEPS_PER_ROTATION) / 360)
 
-        for i in range (0,rots):
-            time.sleep(SLEEP)
-            if theta > 0:
-                self._left.run(Adafruit_MotorHAT.FORWARD)
-                self._right.run(Adafruit_MotorHAT.BACKWARD)
-            else:
-                self._left.run(Adafruit_MotorHAT.BACKWARD)
-                self._right.run(Adafruit_MotorHAT.FORWARD)
-            time.sleep(abs(45)*.01158)
-            self.stop()
-
-        if theta > 0:
-            self._left.run(Adafruit_MotorHAT.FORWARD)
-            self._right.run(Adafruit_MotorHAT.BACKWARD)
+        print('theta =', theta, 'steps =',steps)
+        if steps > 0:
+            self.right(steps)
         else:
-            self._left.run(Adafruit_MotorHAT.BACKWARD)
-            self._right.run(Adafruit_MotorHAT.FORWARD)
-        time.sleep(abs(rem)*SCALE)
-        self.stop()
+            self.left(abs(steps))
 
-        time.sleep(SLEEP)
+
+    def cleanup(self):
+        GPIO.cleanup()
+        self.turnOffMotors()
+
+
+class Led:
+
+    def __init__(self, pin_num):
+        GPIO.setmode(GPIO.BOARD)
+        self.pin = pin_num
+        self.status = 'off'
+        GPIO.setup(self.pin, GPIO.OUT)
+
+    def on(self):
+        self.status = 'on'
+        GPIO.output(self.pin, GPIO.HIGH)
+
+    def off(self):
+        self.status = 'off'
+        GPIO.output(self.pin, GPIO.LOW)
+
+
+
+class Marker:
+
+    def __init__(self, pin_num, pred, pgreen, pblue):
+        GPIO.setmode(GPIO.BOARD)
+        self.pin = pin_num
+        self.status = 'down'
+        GPIO.setup(self.pin, GPIO.OUT)
+        self.red = Led(pred)
+        self.green = Led(pgreen)
+        self.blue = Led(pblue)
+
+    def standby(self):
+        GPIO.output(self.pin, GPIO.LOW)
+        self.red.off()
+        self.green.off()
+        self.blue.on()
+
+    def down(self):
+        GPIO.output(self.pin, GPIO.LOW)
+        self.status = 'down'
+        self.red.off()
+        self.blue.off()
+        self.green.on()
+
+    def up(self):
+        GPIO.output(self.pin, GPIO.HIGH)
+        self.status = 'up'
+        self.blue.off()
+        self.green.off()
+        self.red.on()
